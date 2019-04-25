8a003ee74c48915ba81ea9fb0dcd8101c92a325e
hoang <vinhyenvodoi98@gmail.com>
2019-04-25 11:13:41 +0700

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage")
+const Voting = artifacts.require("Voting")
 
 module.exports = function(deployer){
-    deployer.deploy(SimpleStorage);
+    deployer.deploy(Voting);
 };
